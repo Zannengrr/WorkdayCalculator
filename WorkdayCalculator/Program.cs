37f51a3d15b9cad366a8dde0c266bc9346305bac
Ivan_Horvatin <i.horvatin86@gmail.com>
2023-07-18 00:35:29 +0200

    Add project files.

---
+++ WorkdayCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WorkdayCalculator.Interfaces;
+using WorkdayCalculator.Services;
+
+string format = "dd-MM-yyyy HH:mm";
+IWorkdayCalendar calendar = new WorkdayCalendar();
+calendar.SetWorkdayStartAndStop(8, 0, 16, 0);
+calendar.SetRecurringHoliday(5, 17);
+calendar.SetHoliday(new DateTime(2004, 5, 27));
+var start = new DateTime(2004, 5, 24, 18, 5, 0);
+var increment = -5.5m;
+var incrementedDate = calendar.GetWorkdayIncrement(start, increment);
+
+Console.WriteLine($"{start.ToString(format)} with an addition of {increment} work days is {incrementedDate.ToString(format)}");
---
+++ WorkdayCalculator/Services/WorkdayCalendar.cs
@@ -0,0 +1,127 @@
+﻿using WorkdayCalculator.Interfaces;
+
+namespace WorkdayCalculator.Services
+{
+    public class WorkdayCalendar : IWorkdayCalendar
+    {
+        private readonly HashSet<DateTime> holidays;
+        private readonly HashSet<(int month, int day)> recurringHolidays;
+        private TimeSpan startWorkingHours;
+        private TimeSpan endWorkingHours;
+        private TimeSpan workingHours;
+
+        public WorkdayCalendar()
+        {
+            holidays = new HashSet<DateTime>();
+            recurringHolidays = new HashSet<(int, int)>();
+        }
+
+        public DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays)
+        {
+            int sign = Math.Sign(incrementInWorkdays);
+            int days = (int)Math.Abs(incrementInWorkdays);
+            decimal restDecimal = Math.Abs(incrementInWorkdays - Math.Truncate(incrementInWorkdays));
+            decimal restHours = workingHours.Hours * restDecimal;
+            decimal restMinutes = Math.Floor((restHours - Math.Truncate(restHours)) * 60);
+
+            DateTime currentDate = startDate.Date;
+
+            if (sign == 1)
+            {
+                if (startDate.Hour < endWorkingHours.Hours)
+                {
+                    int hoursInWorkDayLeft = startDate.Hour > startWorkingHours.Hours ? endWorkingHours.Hours - startDate.Hour : workingHours.Hours;
+                    if (hoursInWorkDayLeft < restHours)
+                    {
+                        restHours = Math.Abs(restHours - hoursInWorkDayLeft) + 24;
+                    }
+                    if(startDate.Hour >= startWorkingHours.Hours) restMinutes += startDate.Minute;
+
+                }
+                else
+                {
+                    currentDate = GetNextOrPreviousWorkday(currentDate, sign);
+                }
+            }
+            else
+            {
+                if (startDate.Hour > startWorkingHours.Hours)
+                {
+                    int hoursInWorkDayLeft = startDate.Hour < endWorkingHours.Hours ? startWorkingHours.Hours - startDate.Hour : workingHours.Hours;
+                    if (hoursInWorkDayLeft < restHours)
+                    {
+                        restHours = Math.Abs(restHours - hoursInWorkDayLeft) + 24;
+                    }
+                    if(startDate.Hour <= endWorkingHours.Hours) restMinutes = 60 - restMinutes;
+                }
+                else
+                {
+                    currentDate = GetNextOrPreviousWorkday(currentDate, sign);
+                }
+            }
+
+
+            while (days > 0)
+            {
+                currentDate = GetNextOrPreviousWorkday(currentDate, sign);
+                days--;
+            }
+
+            if(sign == 1) currentDate += new TimeSpan((int)restHours,(int)restMinutes, 0) + startWorkingHours;
+            else currentDate += new TimeSpan(sign*(int)restHours, sign*(int)restMinutes, 0) + endWorkingHours;
+
+            return currentDate;
+        }
+
+        public void SetHoliday(DateTime date)
+        {
+            holidays.Add(date.Date);
+        }
+
+        public void SetRecurringHoliday(int month, int day)
+        {
+            recurringHolidays.Add((month, day));
+        }
+
+        public void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes)
+        {
+            startWorkingHours = new TimeSpan(startHours, startMinutes, 0);
+            endWorkingHours = new TimeSpan(stopHours, stopMinutes, 0);
+            workingHours = endWorkingHours - startWorkingHours;
+        }
+
+        private DateTime GetNextOrPreviousWorkday(DateTime date, int sign)
+        {
+            DateTime nextDate = date;
+            do
+            {
+                nextDate = nextDate.AddDays(sign);
+            }
+            while (!IsWorkday(nextDate));
+
+            return nextDate;
+        }
+
+        private bool IsWorkday(DateTime date)
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return false;
+
+            if (holidays.Contains(date))
+            {
+                Console.WriteLine("Holdiays found");
+                return false;
+            }
+
+            foreach (var holiday in recurringHolidays)
+            {
+                if (date.Month == holiday.month && date.Day == holiday.day)
+                {
+                    Console.WriteLine("Holdiays MATHC FOUND");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
